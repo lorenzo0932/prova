711423abc5514ca7a37a8b809a156dd1128fe865
lorenzo0932 <lorenzo0932@gmail.com>
2020-11-10 13:31:54 +0100

    init

---
+++ Untitled-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main () {
+    printf("Porcodio\n");
+    int x;
+    scanf("%d", &x);
+    printf("%d Porcodio\n", x);
+
+    return 0;
+    }
+
+    Porcocane la bestemmie
+    
